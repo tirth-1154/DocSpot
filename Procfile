de35d46dfb8ece9165b163dfaaf8d646051065e2
tirth-1154 <panchaltirth1105@gmail.com>
2026-03-13 15:25:39 +0530

    Add Procfile for deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn DocSpot.wsgi
--- carespot/settings.py
+++ carespot/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-j8vgi^1ojsar-uus%=g-v=egl^+m!=i%bf8w#a1^h_kusv5f+8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['docspot.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
